4fcc647d0404b4e70f6d6ab108bab6dfb7b9fdd9
turizsolt <turi.zsolt.m@gmail.com>
2016-12-07 12:24:51 +0100

    Backup get route.

--- routes/index.ts
+++ routes/index.ts
@@ -2,10 +2,17 @@
  * Created by zsiri on 2016.12.05..
  */
 
+import {Questions} from "../app/questions";
+var questions = Questions.getInstance();
+
 export function defineRoutes(router) {
 
     router.get('/', function(req, res) {
         res.sendFile('index.html');
+    });
+
+    router.get('/get', function(req, res) {
+        res.send(questions.getRandomQuestion());
     });
 
     return router;
